f6c43933fbdc918520b5c2b19a7370a337cdb6c5
Diba Mirza <dimirza@cs.ucsb.edu>
2020-03-17 13:08:39 -0700

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,40 @@
+// Write your name
+// Write your perm number
+// Do not include a main function in this file
+// Only submit this file
+// See code.h for the descriptions of each problem
+#include "code.h"
+
+
+/* Problem 1: 10 pts */
+ 
+TeaPacket* bestPacket(TeaPacket* head){
+   // TO DO
+   return 0;
+}
+
+
+
+/* Problem 2: 10 points*/
+
+Node* insert(Node* head, int value){
+   // TO DO
+   return;
+}
+
+/* Problem 3: 10 points*/
+
+bool isBalanced(string s){
+   // TO DO
+   return true;
+}
+
+
+/* Problem 4: 10 points*/
+
+Person* priorityLinkedList(Person* head){
+   // TO DO
+   return 0;
+   
+}
+
